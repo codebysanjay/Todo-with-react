4849bece0e55c1a79a3a1c6f3b38d047bb8973ad
codebysanjay <sanjaykakkatiri@gmail.com>
2020-11-18 00:15:14 +0530

    Master

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./header.css";
+import{Link}from "react-router-dom";
+
 export default function Header() {
     return (
         <header>
-            <a href="/"  >Home</a>
-            <a href="/about" >About</a>
+            <Link to="/"  >Home</Link>
+            <Link to="/about" >About</Link>
         </header>
     )
 }
